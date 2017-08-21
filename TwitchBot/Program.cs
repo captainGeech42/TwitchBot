ca19e8a4c5af3e8b54d15620eec5ced91e44de0d
Zander Work <zander@zanderwork.com>
2017-08-20 19:31:01 -0700

    initial commit

---
+++ TwitchBot/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Example chat input: :shiftix!shiftix@shiftix.tmi.twitch.tv PRIVMSG #techgod52 :d
+ *                        ^        ^       ^                             ^        ^
+ *                        ----username------                           channel | message
+ * 
+ */
+
+namespace TwitchBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
